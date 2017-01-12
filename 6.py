a39d8c2fb875e3a41d2463f37ca7f58cab98d24b
Malcolm Gin <malcolm.gin@gmail.com>
2017-01-11 17:03:48 -0800

    Took notes on approach so far with 6.py.

---
+++ 6.py
@@ -0,0 +1,15 @@
+# Start URL: http://www.pythonchallenge.com/pc/def/channel.html
+# Look at source. See comment: zip
+# Open URL: http://www.pythonchallenge.com/pc/def/channel.zip
+# Download channel.zip and look at structure.
+# There is a readme. Which reads:
+#   welcome to my zipped list.
+#    
+#   hint1: start from 90052
+#   hint2: answer is inside the zip
+# Guess on approach:
+# Use some zip libraries to navigate through zip for info in files, starting with 90052.txt, and find
+# answer. Sort of like 6.py but with zip archive handling.
+# 90052.txt within ZIP archive reads:
+#   Next nothing is 94191 
+
