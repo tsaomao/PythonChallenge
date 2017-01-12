15ab4eee3086baf5128d6aeaffc5b140d084178b
Malcolm Gin <malcolm.gin@gmail.com>
2017-01-11 22:15:08 -0800

    Finalized 6.py

--- 6.py
+++ 6.py
@@ -12,4 +12,33 @@
 # answer. Sort of like 6.py but with zip archive handling.
 # 90052.txt within ZIP archive reads:
 #   Next nothing is 94191 
+import zipfile
+import re
 
+# Handle on zip file "channel.zip"
+f = zipfile.ZipFile("channel.zip", "r")
+
+# Start filename
+filenum = 90052
+collcomments = ""
+
+# Crashes at filenum = 46145 (iteration 908)
+# Because the yield is 'Collect the comments', retroactively collect
+# comments in order
+for x in range(0, 908):
+	filecont = f.read("{}.txt".format(str(filenum))).decode("utf-8")
+	nothing = re.findall('Next nothing is ([0-9]*)', filecont)
+	filenum = int(nothing[0])
+	collcomments += f.getinfo("{}.txt".format(filenum)).comment.decode("utf-8")
+	print('retrieval: {}, full text: {}, nothing: {}'.format(x, filecont, filenum))
+
+
+filefincont = str(f.read("{}.txt".format(str(46145))))
+print(filefincont)
+
+print(collcomments)
+
+print("http://www.pythonchallenge.com/pc/def/{}.html".format("hockey"))
+
+# Yields clue, to look at the letters that make up the HOCKEY banner
+print("SOLUTION: http://www.pythonchallenge.com/pc/def/{}.html".format("oxygen"))
