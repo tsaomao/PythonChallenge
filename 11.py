2372b9a28763938f7058828c8bc742ae08080c68
Malcolm Gin <malcolm.gin@gmail.com>
2017-01-25 15:49:52 -0800

    Solved 11. Started 12.

--- 11.py
+++ 11.py
@@ -5,4 +5,29 @@
 # 	<font color="gold" size="+1"></font>
 # Split image by odd and even
 # Image: http://www.pythonchallenge.com/pc/return/cave.jpg
+# Developed and tested in iOS Pythonista app
+# photos.pick_image() chooses from Photo library
+# Image.save() saves to App's local virtual filesystem
+from PIL import Image
+import photos
 
+im = photos.pick_image()
+(w, h) = im.size
+
+even = Image.new('RGB', (w // 2, h // 2))
+odd = Image.new('RGB', (w // 2, h // 2))
+
+for i in range(w):
+    for j in range(h):
+        p = im.getpixel((i,j))
+        if (i+j)%2 == 1:
+            odd.putpixel((i // 2, j // 2), p)
+        else:
+            even.putpixel((i // 2, j // 2), p)
+even.save('even.jpg')
+odd.save('odd.jpg')
+
+even.show()
+odd.show()
+
+print("SOLUTION: http://www.pythonchallenge.com/pc/return/{}.html".format("evil"))
---
+++ 12.py
@@ -0,0 +1,16 @@
+# Level: 12
+# Page URL: http://www.pythonchallenge.com/pc/return/evil.html
+# Username: huge
+# Password: file
+# Graphic URL: http://www.pythonchallenge.com/pc/return/evil1.jpg
+# Try: http://www.pythonchallenge.com/pc/return/evil2.jpg
+# Message reads "not jpg - _.gfx"
+# Try: http://www.pythonchallenge.com/pc/return/evil2.gfx
+# Download failed.
+# Try: http://www.pythonchallenge.com/pc/return/evil3.jpg
+# Message reads "no more evils"
+# Try: http://www.pythonchallenge.com/pc/return/evil4.jpg
+# Investigating further, this seems to be a dead end
+# Corrupted image? (not HTTP 404)
+# Cards have "5" on them, so there are probably 5 somethings.
+# Go back to evil2.gfx and look at it. Deal into 5 different files?
